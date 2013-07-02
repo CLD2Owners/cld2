f70f2fd4e5c5a8d3e0d454a1aedb04755fff04cd
dsites@google.com <dsites@google.com@b252ecd4-b096-bf77-eb8e-91563289f87e>
2013-07-02 22:56:47 +0000

    Add license
    git-svn-id: https://cld2.googlecode.com/svn/trunk@5 b252ecd4-b096-bf77-eb8e-91563289f87e

--- internal/cld_generated_cjk_delta_bi_4.cc
+++ internal/cld_generated_cjk_delta_bi_4.cc
@@ -1,5 +1,18 @@
-// Copyright 2013 Google Inc.
 //
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Created by postproc-cld2 4.0 on 2013-06-14 17:07:50
 // From command line:
 //  --cld2 --cc --just_read_raw --delta_bi
@@ -23,6 +36,7 @@
 //  --nodoubleclose --langcounts --writebin --list_items=120
 //  i18n/encodings/cld2/prob_data/vetted_bigram_prob_20130614_sort.utf8
 //
+
 #include "cld2tablesummary.h"
 namespace CLD2 {
 
