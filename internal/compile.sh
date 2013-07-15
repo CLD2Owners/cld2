f0d23f38f1377bd8d55293f6de1cd513529b09e9
dsites@google.com <dsites@google.com@b252ecd4-b096-bf77-eb8e-91563289f87e>
2013-07-15 21:59:06 +0000

    Fix small oversight in quadgram table
    git-svn-id: https://cld2.googlecode.com/svn/trunk@9 b252ecd4-b096-bf77-eb8e-91563289f87e

--- internal/compile.sh
+++ internal/compile.sh
@@ -22,7 +22,7 @@ g++ -O2 -m64  compact_lang_det_test.cc \
   tote.cc utf8statetable.cc  \
   cld_generated_cjk_uni_prop_80.cc cld2_generated_cjk_compatible.cc  \
   cld_generated_cjk_delta_bi_4.cc generated_distinct_bi_0.cc  \
-  cld2_generated_quadchrome0604.cc cld2_generated_deltaoctachrome0614.cc \
+  cld2_generated_quadchrome0715.cc cld2_generated_deltaoctachrome0614.cc \
   cld2_generated_distinctoctachrome0604.cc  cld_generated_score_quad_octa_1024_256.cc  \
   -o compact_lang_det_test_chrome0614
 echo "  compact_lang_det_test_chrome0614 compiled"
@@ -35,7 +35,7 @@ g++ -O2 -m64  cld2_unittest.cc \
   tote.cc utf8statetable.cc  \
   cld_generated_cjk_uni_prop_80.cc cld2_generated_cjk_compatible.cc  \
   cld_generated_cjk_delta_bi_4.cc generated_distinct_bi_0.cc  \
-  cld2_generated_quadchrome0604.cc cld2_generated_deltaoctachrome0614.cc \
+  cld2_generated_quadchrome0715.cc cld2_generated_deltaoctachrome0614.cc \
   cld2_generated_distinctoctachrome0604.cc  cld_generated_score_quad_octa_1024_256.cc  \
   -o cld2_unittest
 echo "  cld2_unittest compiled"
@@ -48,7 +48,7 @@ g++ -O2 -m64  -Davoid_utf8_string_constants cld2_unittest.cc \
   tote.cc utf8statetable.cc  \
   cld_generated_cjk_uni_prop_80.cc cld2_generated_cjk_compatible.cc  \
   cld_generated_cjk_delta_bi_4.cc generated_distinct_bi_0.cc  \
-  cld2_generated_quadchrome0604.cc cld2_generated_deltaoctachrome0614.cc \
+  cld2_generated_quadchrome0715.cc cld2_generated_deltaoctachrome0614.cc \
   cld2_generated_distinctoctachrome0604.cc  cld_generated_score_quad_octa_1024_256.cc  \
   -o cld2_unittest_avoid
 echo "  cld2_unittest -Davoid_utf8_string_constants compiled"
