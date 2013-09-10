4109ba42b9900c42d00747a764405e96c7e83162
dsites@google.com <dsites@google.com@b252ecd4-b096-bf77-eb8e-91563289f87e>
2013-09-10 16:50:23 +0000

    remove mistaken main program from library build
    git-svn-id: https://cld2.googlecode.com/svn/trunk@80 b252ecd4-b096-bf77-eb8e-91563289f87e

--- internal/compile_libs.sh
+++ internal/compile_libs.sh
@@ -14,7 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-g++ -shared -fPIC -O2 -m64  compact_lang_det_test.cc \
+g++ -shared -fPIC -O2 -m64 \
   cldutil.cc cldutil_shared.cc compact_lang_det.cc compact_lang_det_hint_code.cc \
   compact_lang_det_impl.cc  debug.cc fixunicodevalue.cc \
   generated_entities.cc  generated_language.cc generated_ulscript.cc  \
@@ -26,7 +26,7 @@ g++ -shared -fPIC -O2 -m64  compact_lang_det_test.cc \
   cld2_generated_distinctoctachrome0604.cc cld_generated_score_quad_octa_1024_256.cc  \
   -o libcld2.so
 
-g++ -shared -fPIC -O2 -m64  compact_lang_det_test.cc \
+g++ -shared -fPIC -O2 -m64 \
   cldutil.cc cldutil_shared.cc compact_lang_det.cc compact_lang_det_hint_code.cc \
   compact_lang_det_impl.cc  debug.cc fixunicodevalue.cc \
   generated_entities.cc  generated_language.cc generated_ulscript.cc  \
