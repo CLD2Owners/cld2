85f0ee72b21ee7009a76476c2c55eafa30f6a378
andrewhayden@google.com <andrewhayden@google.com@b252ecd4-b096-bf77-eb8e-91563289f87e>
2014-11-07 10:16:57 +0000

    Add Apache license header to 20141015 sources.
    git-svn-id: https://cld2.googlecode.com/svn/trunk@193 b252ecd4-b096-bf77-eb8e-91563289f87e

--- internal/cld2_generated_distinctoctachrome.cc
+++ internal/cld2_generated_distinctoctachrome.cc
@@ -1,5 +1,17 @@
 // Copyright 2014 Google Inc. All Rights Reserved.
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Created by postproc-cld2 4.0 on 2014-10-16 11:28:58
 // From command line:
 //  --cld2 --cc --just_read_raw --distinct_octa --standard --minchars=2 
